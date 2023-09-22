f0681def975ccb861c83fb387836f45c6340987c
Vincent123456789101112 <vincents2026@student.cis.edu.hk>
2023-09-22 11:22:49 +0800

    -

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+/* I Vincent, have have neither given nor received unauthorized aid on this piece of work.*/
+
 import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favoriteFood;
 
+        System.out.println("Enter your age:");
+        age = scanner.nextInt();
+        scanner.nextLine();
 
+        System.out.println("Enter your first name:");
+        firstName = scanner.nextLine();
 
+        System.out.println("Enter your favorite food:");
+        favoriteFood = scanner.nextLine();
 
+        System.out.println("First name: " + firstName + "\nAge: " + age + "\nFavorite food: " + favoriteFood);
     }
-
 }
